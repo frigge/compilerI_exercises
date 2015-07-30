398737d4b70f40ef318da007922c5d899486fcbe
NeoRoy <carsten.burmeister@googlemail.com>
2015-07-30 22:20:37 +0200

    Piglet is alive
    - Erzeugt jetzt interpretierbaren Code
    - Entfernt Probleme mit dem CJump (falsch gelesen: Dachte der jumpt auf
    true, aber er springt auf false)
    - Exp.invers() hinzugefügt (berechnet für Booleans das inverse).
    - Mehrere Kommentare hinzugefügt, um immerhin ein bisschen den Piglet
    Code lesen zu können
    - Probleme mit der Offsetberechnung behoben

--- tests/MainTestForPigletWithClassDecl.java
+++ tests/MainTestForPigletWithClassDecl.java
@@ -3,10 +3,11 @@ class MainTest{
     	Integer n;
     	int sum;
     	int useless;
-    	
+
     	n = new Integer();
     	useless = n.set(5);
     	sum=0;
+    	System.out.println(n.get());
     	while(0<n.get()){
     		sum = sum + n.get();
     		useless = n.set(n.get()-1);
