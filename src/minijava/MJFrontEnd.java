c543273b3068d13b6c932ba4a5e3f67f225b9b99
NeoRoy <carsten.burmeister@googlemail.com>
2015-09-30 18:23:53 +0200

    Fixes MIPS

--- src/minijava/MJFrontEnd.java
+++ src/minijava/MJFrontEnd.java
@@ -12,12 +12,20 @@ class MJFrontEnd {
 //      inputFileName = "tests/carFrigge/MainTest.java";
 //      inputFileName = "tests/carFrigge/MainTestForLookup.java";
 //      inputFileName = "tests/carFrigge/MainTestForType.java";
-	    inputFileName = "tests/carFrigge/MainTestForPiglet.java";
+//	    inputFileName = "tests/carFrigge/MainTestForPiglet.java";
 //      inputFileName = "tests/carFrigge/MainTestForPigletWithClassDecl.java";
 //     inputFileName = "tests/carFrigge/MainTestForPigletWithMultipleClassDecls.java";
 //	     inputFileName = "tests/carFrigge/MainTestForKangaSpill.java";
 //	     inputFileName = "tests/carFrigge/MainTestForKangaParameter.java";
 //      inputFileName = "tests/carFrigge/MainTestForPigletArray.java";
+//	   inputFileName = "tests/Hauke/factorial.java";
+	   inputFileName = "tests/Hauke/TreeVisitor.java";
+//	   inputFileName = "tests/Hauke/QuickSort.java";
+//	   inputFileName = "tests/carFrigge/ArrayList.java";
+//	   inputFileName = "tests/carFrigge/InnerIf3.java";
+//	   inputFileName = "tests/carFrigge/LinearSearch2.java";
+//	    inputFileName = "tests/carFrigge/MT.java";
+//	    inputFileName = "tests/carFrigge/NullTest.java";
     }
    else {
       System.out.println("MJFrontEnd: starting on file " + args[0]);
@@ -39,7 +47,7 @@ class MJFrontEnd {
       }
       */
       // Print the resulting AST on standard output.
-//      System.out.println(ast.print().getString());
+//      System.out.println(ast.print().getString()); 
 //      System.out.println(ast.validate().size()); 
       ast.check();
 //      System.out.println("Anzahl der Fehler: "+ast.error().size());
@@ -54,15 +62,11 @@ class MJFrontEnd {
     	  spiglet.Program spiglet = piglet.toSpiglet();
 //    	  System.out.println(spiglet.print().getString());
     	  kanga.Program kanga = spiglet.toKanga();
-    	  System.out.println(kanga.print().getString());
-
-          System.out.println("");
-          System.out.println("");
-          System.out.println("");
-          System.out.println("");
+//    	  System.out.println(kanga.print().getString());
 //    	  kanga.execute();
-          mips.Program mips = kanga.toMips();
-          System.out.println(mips.print());
+    	  mips.Program mips = kanga.toMips();
+    	  System.out.println(mips.print());
+//    	  mips.execute();
       }
     }
     catch (FileNotFoundException e) {
